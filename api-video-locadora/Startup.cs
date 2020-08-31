f770bf90d33faaa640a852d253bbc1bb243e6b47
carolinadiasdc <carolinadiasdc@gmail.com>
2020-08-30 21:36:00 -0300

    feature: swagger

--- api-video-locadora/Startup.cs
+++ api-video-locadora/Startup.cs
@@ -2,38 +2,69 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api_video_locadora.CrossCutting.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace api_video_locadora
 {
     public class Startup
+
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public IConfiguration Conf { get; }
+        public Startup(IConfiguration conf)
         {
+            Conf = conf;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddBootStrapperIoC();
+            services.AddCors();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "API - Video Locadora",
+                    Description = "API REST Video Locadora",
+                    Contact = new OpenApiContact()
+                    {
+                        Name = "Ana Carolina Dias",
+                        Email = "carolinadiasdc@gmail.com",
+                        Url = new Uri("https://github.com/carolinadiasdc")
+                    },
+                });
+
+                c.EnableAnnotations();
+
+            });
+
+        }
+
+
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
+            app.UseCors();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API - Video Locadora v1");
             });
         }
     }
